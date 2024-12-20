2147dbc82e245edf5cc01e1dbacf34c4da44abf5
hggnomm <hggnomm.dev@gmail.com>
2024-12-20 15:56:08 +0700

    layout, nested layout

---
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProductDetailsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <h2>Inner layout</h2>
+      {children}
+    </>
+  );
+}
