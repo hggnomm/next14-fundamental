82a71e593af05ecbcbdbed51df6f217bab101a2d
hggnomm <hggnomm.dev@gmail.com>
2024-12-20 16:19:51 +0700

    dynamic metadata with asynchronous

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,23 @@
-export default function ProductDetail({
+import { Metadata } from "next";
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: { productId: string };
-}) {
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Iphone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+export default function ProductDetail({ params }: Props) {
   return (
     <>
       <h1>Product Nam Dep Trai {params.productId}</h1>
